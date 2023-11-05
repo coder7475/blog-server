33b570f134206d5f28917ec8430788b62a853319
coder7475 <robiulhossain7475@gmail.com>
2023-11-05 11:00:59 +0600

    feat: add a api to get all blogs

--- index.js
+++ index.js
@@ -18,13 +18,26 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const progDB = client.db('Prog-blogs');
+    const allBlogsCol = progDB.collection('allBlogs');
+
+    // get all blogs 
+    app.get('/api/v1/allBlogs', async(req, res) => {
+      const allBlogs = await allBlogsCol.find().toArray();
+
+      res.send(allBlogs);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
