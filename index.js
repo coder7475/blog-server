9e629302ed921d00e4fdd5bb150086be7dac7745
coder7475 <robiulhossain7475@gmail.com>
2023-11-05 10:33:20 +0600

    feat: connect to mongodb using enviromental variables

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
+const dotenv = require('dotenv');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+dotenv.config()
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+// Mongodb Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xjslrno.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
